6abc43fa69ef40ecc8f4db94fd205245659b6aab
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-07 19:47:11 +0300

    m

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ olduğundan bahsetmek gerekir
 
 filename = "./model/cart_model.sav"
 
-loaded_model = pickle.load(open(filename, 'rb'))
+loaded_model = pickle.load(filename)
 
 pxid = st.number_input("Dosya No;", step = False)
 
